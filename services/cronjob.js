5d5ca2f05b78268d877ae8542cdc019da6abf5e3
jguitton <julien.guitton@seitenbau.com>
2019-12-19 14:26:27 +0100

    Fix cron schedule which won't take seconds into account

--- services/cronjob.js
+++ services/cronjob.js
@@ -3,7 +3,7 @@
 const CronJob = require('cron').CronJob;
 
 module.exports = (fetchService) => {
-    return new CronJob('5 */1 * * * *', async function() {
+    return new CronJob('* 5 */1 * * *', async function() {
         let fetchedGames = await fetchService.fetchAndStore();
         console.log('fetch and stored' + fetchedGames + ' games')
     }, null, true, '');
